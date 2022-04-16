b39335dab7c1a53824e14c483b0c0a9f50c37fdd
lyons811 <lyonws@hawaii.edu>
2022-04-15 15:10:08 -1000

    Initial commit

---
+++ Weight.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.cpp
+/// @version 1.0
+///
+/// @author Lyon Singleton <lyonws@hawaii.edu>
+/// @date   15_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "Weight.h"
+#include <iostream>
+using namespace std ;
---
+++ Weight.h
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_lab12a_fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.h
+/// @version 1.0
+///
+/// @author Lyon Singleton <lyonws@hawaii.edu>
+/// @date   15_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std ;
+
+
+class Weight {
+
+};
+
+
+
